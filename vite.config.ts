9d29791e0d383d79a6efd04319854c1214aa82f2
Christian <bournecreative@gmail.com>
2024-06-24 22:20:14 -0500

    removed dist folder that I had renamed to docs - I initially thought github pages would point to this public directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/bournecreative.github.io/react-text-animations/",
   plugins: [react()],
 })
